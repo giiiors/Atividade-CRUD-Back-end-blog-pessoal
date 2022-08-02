8e8085d30c40c9e1e8e82203c233df8c660d51e8
Gio Ferraz <107887069+giiiors@users.noreply.github.com>
2022-08-02 16:38:48 -0300

    blog pessoal

---
+++ Controler/PostagemController.java
@@ -0,0 +1,27 @@
+package com.generation.blogPessoal.Controler;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogPessoal.model.Postagem;
+import com.generation.blogPessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/Postagens")
+@CrossOrigin("*")
+public class PostagemController {
+
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+}
