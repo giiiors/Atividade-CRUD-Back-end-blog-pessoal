bb2da5e3e710b630da2edcf0a43fc0e70126e534
Gio Ferraz <107887069+giiiors@users.noreply.github.com>
2022-08-07 17:55:41 -0300

    mais uma atualizacao

--- repository/TemaRepository.java
+++ repository/TemaRepository.java
@@ -1,5 +1,14 @@
 package com.generation.blogPessoal.repository;
 
-public interface TemaRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.blogPessoal.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+	
+	
 
 }
