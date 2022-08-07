fb62c26e8d5fccb230f04ea0ecbac4309e838863
Gio Ferraz <107887069+giiiors@users.noreply.github.com>
2022-08-07 18:06:41 -0300

    eu amo codar

--- model/Postagem.java
+++ model/Postagem.java
@@ -8,8 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -38,7 +36,7 @@ public class Postagem {
 	private LocalDateTime data;
 	
 	@ManyToOne
-	@JsonIgnoreProperties("Postagem")
+	@JsonIgnoreProperties("")
 	private Tema tema;
 
 	public Tema getTema() {
