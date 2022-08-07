620eea43ab6583596b28172c33d6064af2459f1c
Gio Ferraz <107887069+giiiors@users.noreply.github.com>
2022-08-07 16:16:20 -0300

    vai

--- model/Postagem.java
+++ model/Postagem.java
@@ -1,7 +1,6 @@
 package com.generation.blogPessoal.model;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +11,8 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
 @Entity
 @Table (name = "Postagem")
 
@@ -30,7 +31,7 @@ public class Postagem {
 	@Size (min = 10, max = 500)
 	private String texto;
 	
-	@Temporal(TemporalType.TIMESTAMP)
+	@UpdateTimestamp
 	private LocalDateTime data;
 
 	public long getId() {
