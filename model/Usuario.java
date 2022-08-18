4a93425bc8e3170b76a47c1e606c835fbaac47bb
Gio Ferraz <107887069+giiiors@users.noreply.github.com>
2022-08-18 14:54:48 -0300

    atualizacao

--- model/Usuario.java
+++ model/Usuario.java
@@ -42,6 +42,17 @@ public class Usuario {
 	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem>postagem;
+	 
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+		
+	}
+	
+	public Usuario() { }
 
 	public Long getId() {
 		return id;
@@ -90,7 +101,6 @@ public class Usuario {
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
 	}
-	
 	
 	
 	
--- repository/UsuarioRepository.java
+++ repository/UsuarioRepository.java
@@ -1,8 +1,10 @@
 package com.generation.blogPessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogPessoal.model.Usuario;
@@ -11,5 +13,7 @@ import com.generation.blogPessoal.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
 	public Optional<Usuario> findByUsuario(String usuario);
+	
+	public List <Usuario>findAllByNomeContainingIgnoreCase(@Param("nome")String nome);
 	
 }
